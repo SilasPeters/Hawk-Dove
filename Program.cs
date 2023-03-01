02b367d370e7c1c5841b46786c18442d233e3e92
SilasPeters <silaspeters03@gmail.com>
2023-03-01 18:06:44 +0100

    Logged seed

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System.Diagnostics;
 using Hawk_Dove;
 
-const int iterations = 10000;
+const int iterations = 1000;
 
 var resource = new DefaultResource(1f);
 var agents   = new Agent[] { new NeutralAgent(), new AggressiveAgent() };
 var hawkDoveScenario = new HawkDoveScenario(resource, agents, 1.2f);
 
-var random = new Random(69420); // Seed ensures deterministic testing
+var seed   = Environment.TickCount;
+var random = new Random(seed); // Seed ensures deterministic testing
 
 var outcomes = new float[iterations];
 var t        = new Stopwatch();
@@ -25,6 +26,7 @@ for (var i = 0; i < iterations; i++)
 // Print summaries
 Console.WriteLine();
 Console.WriteLine($"{iterations} iterations took {t.Elapsed.Milliseconds}ms");
+Console.WriteLine("Seed used: " + seed);
 Console.WriteLine("Average: " + outcomes.Average());
 Console.WriteLine("    Min: " + outcomes.Min());
 Console.WriteLine("    Max: " + outcomes.Max());
