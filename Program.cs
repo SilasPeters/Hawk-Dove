7f93868a002802b38540f73ec2eafa36fae03721
FabianRutten <f.w.rutten@students.uu.nl>
2023-03-20 22:45:24 +0100

    variations (grof)

--- Program.cs
+++ Program.cs
@@ -4,22 +4,37 @@ using Hawk_Dove;
 const int iterations = 1000;
 using var output = new Output();
 
-var resource = new DefaultResource(1f);
-var agents   = new Agent[] { new NeutralAgent(), new AggressiveAgent() };
-var hawkDoveScenario = new HawkDoveScenario(resource, agents, 1.2f);
 
 var seed   = Environment.TickCount;
 var random = new Random(seed); // Seed ensures deterministic testing
 
-var outcomes = new float[iterations];
+
 var t        = new Stopwatch();
 
-for (var i = 0; i < iterations; i++)
+
+
+const int variations = 1000;
+
+var averages = new float[variations];
+var variationVariables = new float[variations];
+
+for (var i = 0; i < variations; ++i)
 {
-	t.Start();
-	var outcome = hawkDoveScenario.Run(random);
-	t.Stop();
-	outcomes[i] = outcome;
+    float variationVariable = 5f * (float)i / variations;
+    var resource = new DefaultResource(1f);
+    var agents = new Agent[] { new NeutralAgent(), new AggressiveAgent() };
+    var hawkDoveScenario = new HawkDoveScenario(resource, agents, variationVariable);
+    var outcomes = new float[iterations];
+    for (var j = 0; j < iterations; j++)
+    {
+        
+        t.Start();
+        var outcome = hawkDoveScenario.Run(random);
+        t.Stop();
+        outcomes[j] = outcome;
+    }
+    averages[i] = outcomes.Average();
+    variationVariables[i] = variationVariable;
 }
 
 // Print summaries
@@ -27,11 +42,11 @@ output.WriteLine("Hawk-Dove Simulation on " + DateTime.Now.ToString("MM-dd HH-mm
 output.WriteLine();
 output.WriteLine($"{iterations} iterations took:", t.Elapsed.Milliseconds.ToString(), "ms");
 output.WriteLine("Seed used:",                     seed.ToString());
-output.WriteLine("Average:",                       outcomes.Average().ToString());
-output.WriteLine("Min:",                       outcomes.Min().ToString());
-output.WriteLine("Max:",                       outcomes.Max().ToString());
+output.WriteLine("Average:",                       averages.Average().ToString());
+output.WriteLine("Min:",                           averages.Min().ToString());
+output.WriteLine("Max:",                       averages.Max().ToString());
 output.WriteLine();
-foreach(var outcome in outcomes)
-	output.WriteLine(outcome.ToString());
+for (var i = 0; i < variations; i++)
+    output.WriteLine(averages[i].ToString() + "," + variationVariables[i].ToString());
 
 Console.Write("Wrote output to " + output.OutputLocation);
