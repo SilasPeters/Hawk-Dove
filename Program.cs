6c70745a9e0232a21b113859a36a7f241eb83d83
SilasPeters <silaspeters03@gmail.com>
2023-03-23 00:16:08 +0100

    Renamed Output.cs

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@ var agents = new Agent[] { new FlexibleAgent(), new FlexibleAgent() };
 var hawkDoveScenario = new HawkDoveScenario(resource, agents, conflictCosts);
 
 // Boilerplate
-using var output = new Output();
-output.WriteLine("Hawk-Dove Simulation on " + DateTime.Now.ToString("MM-dd HH-mm-ss"));
-output.WriteLine("Iterations:", iterations.ToString());
+using var export = new Export();
+export.AddRow("Hawk-Dove Simulation on " + DateTime.Now.ToString("MM-dd HH-mm-ss"));
+export.AddRow("Iterations:", iterations.ToString());
 
 var seed = Environment.TickCount;
 var random = new Random(seed); // Seed ensures deterministic testing
-output.WriteLine("Seed used:", seed.ToString());
+export.AddRow("Seed used:", seed.ToString());
 
 // Algorithm
 for (var i = 0; i < iterations; ++i)
 {
     var outcome = hawkDoveScenario.Run(random);
-    output.WriteLine(outcome.ToString());
+    export.AddRow(outcome.ToString());
 }
